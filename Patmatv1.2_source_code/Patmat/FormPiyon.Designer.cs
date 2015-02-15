700466401e35bd47b6e394c1a9f8149d20200261
Mustafa Çakıcı <glikoz@live.com>
2015-02-15 03:25:58 +0200

    New Release
    düzenli halde dosyalar listelendi.

---
+++ Patmatv1.2_source_code/Patmat/FormPiyon.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Patmat
+{
+  partial class FormPiyon
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPiyon));
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.radioButton_fil = new System.Windows.Forms.RadioButton();
+      this.radioButton_at = new System.Windows.Forms.RadioButton();
+      this.radioButton_kale = new System.Windows.Forms.RadioButton();
+      this.radioButton_vezir = new System.Windows.Forms.RadioButton();
+      this.button1 = new System.Windows.Forms.Button();
+      this.groupBox1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+      this.groupBox1.Controls.Add(this.radioButton_fil);
+      this.groupBox1.Controls.Add(this.radioButton_at);
+      this.groupBox1.Controls.Add(this.radioButton_kale);
+      this.groupBox1.Controls.Add(this.radioButton_vezir);
+      this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+      this.groupBox1.Location = new System.Drawing.Point(12, 12);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(179, 80);
+      this.groupBox1.TabIndex = 0;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Almak İstediğin Taş";
+      // 
+      // radioButton_fil
+      // 
+      this.radioButton_fil.AutoSize = true;
+      this.radioButton_fil.BackColor = System.Drawing.Color.Transparent;
+      this.radioButton_fil.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+      this.radioButton_fil.Location = new System.Drawing.Point(98, 45);
+      this.radioButton_fil.Name = "radioButton_fil";
+      this.radioButton_fil.Size = new System.Drawing.Size(35, 17);
+      this.radioButton_fil.TabIndex = 1;
+      this.radioButton_fil.Text = "Fil";
+      this.radioButton_fil.UseVisualStyleBackColor = false;
+      // 
+      // radioButton_at
+      // 
+      this.radioButton_at.AutoSize = true;
+      this.radioButton_at.BackColor = System.Drawing.Color.Transparent;
+      this.radioButton_at.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+      this.radioButton_at.Location = new System.Drawing.Point(98, 22);
+      this.radioButton_at.Name = "radioButton_at";
+      this.radioButton_at.Size = new System.Drawing.Size(35, 17);
+      this.radioButton_at.TabIndex = 1;
+      this.radioButton_at.Text = "At";
+      this.radioButton_at.UseVisualStyleBackColor = false;
+      // 
+      // radioButton_kale
+      // 
+      this.radioButton_kale.AutoSize = true;
+      this.radioButton_kale.BackColor = System.Drawing.Color.Transparent;
+      this.radioButton_kale.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+      this.radioButton_kale.Location = new System.Drawing.Point(17, 45);
+      this.radioButton_kale.Name = "radioButton_kale";
+      this.radioButton_kale.Size = new System.Drawing.Size(46, 17);
+      this.radioButton_kale.TabIndex = 1;
+      this.radioButton_kale.Text = "Kale";
+      this.radioButton_kale.UseVisualStyleBackColor = false;
+      // 
+      // radioButton_vezir
+      // 
+      this.radioButton_vezir.AutoSize = true;
+      this.radioButton_vezir.BackColor = System.Drawing.Color.Transparent;
+      this.radioButton_vezir.Checked = true;
+      this.radioButton_vezir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+      this.radioButton_vezir.Location = new System.Drawing.Point(17, 22);
+      this.radioButton_vezir.Name = "radioButton_vezir";
+      this.radioButton_vezir.Size = new System.Drawing.Size(48, 17);
+      this.radioButton_vezir.TabIndex = 1;
+      this.radioButton_vezir.TabStop = true;
+      this.radioButton_vezir.Text = "Vezir";
+      this.radioButton_vezir.UseVisualStyleBackColor = false;
+      // 
+      // button1
+      // 
+      this.button1.BackColor = System.Drawing.SystemColors.Control;
+      this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+      this.button1.Location = new System.Drawing.Point(12, 98);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(180, 35);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Terfi Ettir";
+      this.button1.UseVisualStyleBackColor = false;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // FormPiyon
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.Tan;
+      this.ClientSize = new System.Drawing.Size(203, 147);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.groupBox1);
+      this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "FormPiyon";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Patmat";
+      this.Load += new System.EventHandler(this.FormPiyon_Load);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.RadioButton radioButton_fil;
+    private System.Windows.Forms.RadioButton radioButton_at;
+    private System.Windows.Forms.RadioButton radioButton_kale;
+    private System.Windows.Forms.RadioButton radioButton_vezir;
+    private System.Windows.Forms.Button button1;
+  }
+}
---
+++ Patmatv1.2_source_code/Patmat/FormPiyon.cs
@@ -0,0 +1,106 @@
+﻿#region Bilgi: Lisanslama (GPL)
+/*
+*******************************************************************
+*Patmat(Satranç) v1.2 Copyright (C) 2012 Mustafa ÇAKICI ***********
+*İletişim İçin: glikoz@live.com ***********************************
+*Bu uygulama basit bir satranç oyunudur.***************************
+*Uygulama GPL lisansı altında dağıtılmaktadır.*********************
+*******************************************************************
+*
+*******************************************************************
+*This program is free software; you can redistribute it and/or
+'modify it under the terms of the GNU General Public License
+'as published by the Free Software Foundation; either version 2
+*of the License, or (at your option) any later version.
+*
+*This program is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License
+*along with this program; if not, write to the Free Software
+*Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*******************************************************************
+*
+*******************************************************************
+*http://www.gnu.org/copyleft/gpl.html
+*Turkce cevirisi: http://www.belgeler.org/howto/gpl_copy.html
+*******************************************************************
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Patmat
+{
+  public partial class FormPiyon : Form
+  {
+    public FormPiyon()
+    {
+      InitializeComponent();
+    }
+
+    Image terfitasimg = Form1.img[1];
+    private void FormPiyon_Load(object sender, EventArgs e)
+    {
+      //arkaplan rengi
+      this.BackColor = Color.FromArgb(173, 142, 71);
+    }
+
+   
+    private void button1_Click(object sender, EventArgs e)
+    {
+    
+      string secilentas = "Vezir";
+      if (radioButton_at.Checked == true)
+      {
+        secilentas = "At";
+        if (Form1.terfiOyuncu == 1)
+        terfitasimg = Form1.img[1];
+        else if (Form1.terfiOyuncu == 2)
+        terfitasimg = Form1.img[7];
+      }
+      else if (radioButton_fil.Checked == true)
+      {
+        secilentas = "Fil";
+        if (Form1.terfiOyuncu == 1)
+          terfitasimg = Form1.img[2];
+        else if (Form1.terfiOyuncu == 2)
+          terfitasimg = Form1.img[8];
+      }
+      else if (radioButton_kale.Checked == true)
+      {
+        secilentas = "Kale";
+        if (Form1.terfiOyuncu == 1)
+          terfitasimg = Form1.img[0];
+        else if (Form1.terfiOyuncu == 2)
+          terfitasimg = Form1.img[6];
+      }
+      else if (radioButton_vezir.Checked == true)
+      {
+        secilentas = "Vezir";
+        if (Form1.terfiOyuncu == 1)
+          terfitasimg = Form1.img[4];
+        else if (Form1.terfiOyuncu == 2)
+          terfitasimg = Form1.img[10];
+      }
+      //MessageBox.Show(secilentas);
+      if (Form1.terfiOyuncu > 0)
+      {
+        Form1.lbl[Form1.terfiKoor0, Form1.terfiKoor1].Image = terfitasimg;
+        Form1.tas[Form1.terfiKoor0, Form1.terfiKoor1] = Form1.terfiOyuncu + "," + secilentas;
+        Form1.terfiOyuncu = 0;
+        this.Close();
+      }
+    }
+
+
+  }
+}
