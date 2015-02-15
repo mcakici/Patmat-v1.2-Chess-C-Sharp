0b789439d7228eec04ce81e38503a4099cf07672
Mustafa Çakıcı <glikoz@live.com>
2015-02-15 03:38:01 +0200

    Final release

---
+++ Patmatv1.2_source_code/Patmat/Form2.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Patmat
+{
+  partial class Form2
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.label1 = new System.Windows.Forms.Label();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.Location = new System.Drawing.Point(12, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(340, 48);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Patmat adındanda anlaşılacağı gibi bir satranç oyunudur. Oyundaki amacınız normal" +
+    " satranç kurallarına göre hareket edip rakip oyuncunun şahını yemektir. Tabi ken" +
+    "di şahınızıda kollamalısınız.";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(14, 92);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(57, 13);
+      this.label2.TabIndex = 1;
+      this.label2.Text = "Danışman:";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(12, 64);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(59, 13);
+      this.label3.TabIndex = 1;
+      this.label3.Text = "Hazırlayan:";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+      this.label4.Location = new System.Drawing.Point(69, 88);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(152, 18);
+      this.label4.TabIndex = 2;
+      this.label4.Text = "Oğuz Alp Ündeğerli";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+      this.label5.Location = new System.Drawing.Point(69, 60);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(120, 18);
+      this.label5.TabIndex = 2;
+      this.label5.Text = "Mustafa Çakıcı";
+      // 
+      // Form2
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(364, 126);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "Form2";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Patmat - Hakkında";
+      this.Load += new System.EventHandler(this.Form2_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Timer timer1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label5;
+  }
+}
